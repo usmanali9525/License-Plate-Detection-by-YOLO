f7895c7a007392e0f24b5cc96def9fe43a4cf49f
Usman Ali <usmanali9525@gmail.com>
2023-09-22 16:18:53 +0500

    new

--- Custom_Model_Live_Detection.py
+++ Custom_Model_Live_Detection.py
@@ -2,36 +2,42 @@ from ultralytics import YOLO
 import cv2
 from ultralytics.utils.plotting import Annotator
 import easyocr
+import re
 
 reader = easyocr.Reader(['en'])
-model = YOLO('best.pt')
+model = YOLO(r'Models\best.pt')
 
 cap = cv2.VideoCapture(0)
 
 while True:
-    _, frame = cap.read() 
+    _, frame = cap.read()
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = model.predict(img, verbose=False)
 
     for r in results:
         annotator = Annotator(frame)
         boxes = r.boxes
-        
+
         for box in boxes:
             b = box.xyxy[0]
-            c = box.cls
-            label = model.names[int(c)]
+            #c = box.cls
+            #label = model.names[int(c)]
             plate_roi = frame[int(b[1]):int(b[3]), int(b[0]):int(b[2])]
-            
+
             number_plate_results = reader.readtext(plate_roi)
             number_plate_text = ''
             for result in number_plate_results:
-                number_plate_text += result[1] + ' '
-            
-            print(f"Detected: {label}, Text: {number_plate_text}")
-            
-            annotator.box_label(b, label)
-            
+                text = result[1]
+                # Remove special characters using regular expression
+                text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+                number_plate_text += text + ' '
+
+            # Draw the recognized text on the license plate region
+            cv2.putText(frame, number_plate_text, (int(b[0]), int(b[1]) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            annotator.box_label(b)
+
         frame = annotator.result()
         cv2.imshow('YOLO V8 Detection', frame)
     if cv2.waitKey(1) & 0xFF == ord(' '):
